c4ab35111e7c15e65d20649762602d1b7664eb91
jerrytehtin <jerrytehtin@gmail.com>
2024-09-20 12:45:27 -0400

    update sh

--- scripts/amplify-deploy.sh
+++ scripts/amplify-deploy.sh
@@ -1,14 +1,13 @@
 echo "Deploy app $1 branch $2"
 JOB_ID=$(aws amplify start-job --app-id $1 --branch-name $2 --job-type RELEASE | jq -r '.jobSummary.jobId')
-echo "Release started"
+echo "started the job for amplify demo deployment"
 echo "Job ID is $JOB_ID"
-
 JOB_STATUS="$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID | jq -r '.job.summary.status')"
- while [ "$JOB_STATUS" = "RUNNING" -o "$JOB_STATUS" = "PENDIND" ]; do 
+while [ "$JOB_STATUS" = "RUNNING" -o "$JOB_STATUS" = "PENDIND" ]; do 
  JOB_STATUS="$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID | jq -r '.job.summary.status')"
  echo "The Current Job is $JOB_STATUS"
  sleep 1; 
- done
+done
 JOB_STATUS="$(aws amplify get-job --app-id $1 --branch-name $2 --job-id $JOB_ID | jq -r '.job.summary.status')"
 echo "Job finished"
 echo "Job status is $JOB_STATUS"
